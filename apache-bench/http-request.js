f3c36c905033fdb655169a17b6df944ce6eabf69
GuoBin <844922881@qq.com>
2019-07-19 22:02:05 +0800

    request

---
+++ apache-bench/http-request.js
@@ -0,0 +1,26 @@
+const net = require('net')
+const http = require('http')
+const url = require('url')
+
+function request(url) {
+	return new Promise(function (resolve, reject) {
+		const req = http.get(url, function (res) {
+			let data = '';
+			res.resume();
+			res.on('data', function (buff) {
+				data += buff.toString();
+			})
+			res.on('end', function () {
+				resolve(data)
+			})
+			res.on('error', function (reason) {
+				reject(reason)
+			})
+		})
+	})
+
+}
+
+request('http://www.baidu.com').then(res => console.log(res), function (reason) {
+	console.log(reason)
+})
