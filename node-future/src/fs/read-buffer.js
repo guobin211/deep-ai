686aaa43490d6071ca6038a7353952d758ec8021
GuoBin <844922881@qq.com>
2019-07-19 17:42:05 +0800

    fs

--- node-future/src/fs/read-buffer.js
+++ node-future/src/fs/read-buffer.js
@@ -1,17 +1,25 @@
 const fs = require('fs');
 
-const publicPath = '/Users/guobin/idea/node-c/lib/node-future/public/';
+const publicPath = '/Users/guobin/idea/deep-ai/node-future/public/';
 
 const fileBuff1 = fs.readFileSync(publicPath + 'pic1.png');
 const fileBuff2 = fs.readFileSync(publicPath + 'pic2.png');
 
 function compareBuff(buff1, buff2) {
   const diff = new Map();
+  const buff3 = Buffer.alloc(buff1.length);
   for (let i = 0; i < buff1.length; i++) {
     if (buff1[i] !== buff2[i]) {
       diff.set(i, {a: buff1[i], b: buff2[i]});
     }
+    buff3[i] = buff1[i];
   }
+  console.log(buff3);
+  console.log(buff3[1]);
+  fs.writeFileSync(publicPath + 'pic3.png', buff3, {
+    flag: 'w',
+    encoding: 'utf8'
+  });
   return diff;
 }
 
