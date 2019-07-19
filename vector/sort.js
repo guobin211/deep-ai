f302855cfb762c3df4bda4547b0d1fd71104e89d
GuoBin <844922881@qq.com>
2019-07-19 22:01:49 +0800

    sort.js

---
+++ vector/sort.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function generate(count, range) {
+	const res = [];
+	for (let i = 0; i < count; i++) {
+		res.push(Math.random() * range);
+	}
+	return res;
+}
+
+const arr = generate(10000, 1000);
+
+
+function bubbleSort(arr) {
+	let swap;
+	for (let i = 0; i < arr.length - 1; i++) {
+		for (let j = 0; j < arr.length - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				swap = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = swap;
+			}
+		}
+	}
+}
+
+console.time('sort');
+
+bubbleSort(arr);
+
+console.timeEnd('sort')
