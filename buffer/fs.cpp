b5e414658cd578fde2db83b5819fd38476876d47
GuoBin <844922881@qq.com>
2019-07-30 17:46:36 +0800

    over bye bye

--- buffer/fs.cpp
+++ buffer/fs.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+void read_file_sync(string path) {
+	std::cout << path << std::endl;
+
+}
+
 int main(int argc, char *argv[]) {
 	cout << "hello fs module" << endl;
+	string file_path = "/Users/guobin/idea/deep-ai/language.txt";
+	read_file_sync(file_path);
 	return 0;
 }
